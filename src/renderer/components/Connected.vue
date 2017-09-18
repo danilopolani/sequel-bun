f770b12ef32b1adcd8e2361cf721208eefc7d875
Grork Danilo Polani <danilo.polani@gmail.com>
2017-09-18 18:15:07 +0200

    connected mask

---
+++ src/renderer/components/Connected.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="section no-padding is-fullheight">
+
+    <!-- Context menu -->
+    <context-menu ref="ctxMenu" @ctx-open="onCtxOpen">
+      <div class="dropdown is-active">
+        <div class="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            <a href="#" class="dropdown-item">
+              Truncate
+            </a>
+            <hr class="dropdown-divider">
+            <a href="#" class="dropdown-item has-text-danger" @click="deleteConn()">
+              Drop table
+            </a>
+          </div>
+        </div>
+      </div>
+    </context-menu>
+
+    <div class="columns is-fullheight">
+      <aside class="column is-one-quarter menu padding-left">
+        <p class="menu-label has-text-weight-bold">Tables</p>
+        <!--<ul class="menu-list" v-if="$parent.connections.length > 0">
+          <li v-for="(item, i) in $parent.connections" @contextmenu.prevent="$refs.ctxMenu.open($event, {i: i, item: item})">
+            <a @click="connect(item, i)"><button class="button" :class="'is-' + item.color">{{ item.name }}</button></a>
+          </li>
+        </ul>
+        <small class="has-text-grey" v-else>No table found.</small>-->
+      </aside>
+
+      <main class="column no-padding">
+        <router-view ref="connected"></router-view>
+      </main>
+    </div><!-- /.columns -->
+  </div><!-- /.section -->
+</template>
+
+<script>
+  export default {
+    name: 'connected',
+    data: () => ({
+      ctxData: {}
+    }),
+
+    created () {},
+
+    methods: {
+      /**
+       * Handle context menu open
+       *
+       * @param {object} conn
+       */
+      onCtxOpen (conn) {
+        this.ctxData = conn
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/renderer/components/Structure.vue
@@ -0,0 +1,66 @@
+<template>
+  <table class="table is-bordered is-striped is-narrow is-fullwidth">
+    <thead>
+      <tr>
+        <th class="no-border-left">Field</th>
+        <th class="no-border-left">Type</th>
+        <th class="no-border-left">Length</th>
+        <th class="no-border-left">Unsigned</th>
+        <th class="no-border-left">Allow Null</th>
+        <th class="no-border-left">Key</th>
+        <th class="no-border-left">Default</th>
+        <th class="no-border-left">Extra</th>
+        <th class="no-border-left">Comment</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="no-border-left">id</td>
+        <td class="no-border-left">INT</td>
+        <td class="no-border-left">11</td>
+        <td class="no-border-left">[x]</td>
+        <td class="no-border-left">[ ]</td>
+        <td class="no-border-left">PRIMARY</td>
+        <td class="no-border-left"></td>
+        <td class="no-border-left">auto_increment</td>
+        <td class="no-border-left"></td>
+      </tr>
+      <tr>
+        <td class="no-border-left">user_id</td>
+        <td class="no-border-left">VARCHAR</td>
+        <td class="no-border-left">255</td>
+        <td class="no-border-left">[ ]</td>
+        <td class="no-border-left">[x]</td>
+        <td class="no-border-left"></td>
+        <td class="no-border-left">NULL</td>
+        <td class="no-border-left">None</td>
+        <td class="no-border-left"></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'structure',
+    data: () => ({
+      ctxData: {}
+    }),
+
+    created () {},
+
+    methods: {
+      /**
+       * Handle context menu open
+       *
+       * @param {object} conn
+       */
+      onCtxOpen (conn) {
+        this.ctxData = conn
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
