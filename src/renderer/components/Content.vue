d5c4791a4081d97804afde486ed7e061e269cd71
Grork Danilo Polani <danilo.polani@gmail.com>
2017-10-06 17:10:25 +0200

    removed vue popper and popover

--- src/renderer/components/Content.vue
+++ src/renderer/components/Content.vue
@@ -149,36 +149,11 @@
               <i class="fa fa-caret-left"></i>
             </span>
           </a>
-          <div class="dropdown is-active">
-            <div class="dropdown-trigger">
-              <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                <span class="icon is-small">
-                  <i class="fa fa-cog"></i>
-                  <i class="fa fa-angle-up" aria-hidden="true"></i>
-                </span>
-              </button>
-            </div>
-            <div class="dropdown-menu" id="dropdown-menu" role="menu">
-              <div class="dropdown-content">
-                <a href="#" class="dropdown-item">
-                  Dropdown item
-                </a>
-                <a class="dropdown-item">
-                  Other dropdown item
-                </a>
-                <a href="#" class="dropdown-item is-active">
-                  Active dropdown item
-                </a>
-                <a href="#" class="dropdown-item">
-                  Other dropdown item
-                </a>
-                <hr class="dropdown-divider">
-                <a href="#" class="dropdown-item">
-                  With a divider
-                </a>
-              </div>
-            </div>
-          </div>
+          <a class="button">
+            <span class="icon is-small">
+              <i class="fa fa-cog"></i>
+            </span>
+          </a>
           <a class="button no-border-right" :disabled="page === pages">
             <span class="icon is-small">
               <i class="fa fa-caret-right"></i>
